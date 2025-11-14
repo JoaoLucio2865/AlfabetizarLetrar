d83f5a1d29efd1230c9d2bbdff0ec5f12f2ce73a
João Paulo Lucio Aparecido <japarecido@brasilcard.net>
2025-11-14 13:37:44 -0300

    fix: env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',  // URL do backend
+  baseURL: 'https://backend-alfabetizacao.squareweb.app/public/api',  // URL do backend
   headers: {
     'Content-Type': 'application/json',
   },
